7ea6db801ed41cfa03eb459db8646da4673f84c5
Andraž Pencelj <andrazpencelj@gmail.com>
2013-11-19 18:00:41 +0100

    project commit

---
+++ SoundFrequency/src/main/java/com/andrazpencelj/soundfrequency/HelpActivity.java
@@ -0,0 +1,30 @@
+package com.andrazpencelj.soundfrequency;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by Andraž on 6.11.2013.
+ */
+public class HelpActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
